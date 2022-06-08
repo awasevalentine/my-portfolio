201f1d8ebe906e2f5a15b58f375fa718d935ce5d
awasevalentine <54159265+awasevalentine@users.noreply.github.com>
2022-06-08 22:07:46 +0100

    Added another project to the list of existing projects

---
+++ src/app/Pages/portfolio/data.ts
@@ -0,0 +1,47 @@
+
+export const PortfolioData =[
+  {
+    title: 'Appointment Manager',
+    description: `Apointment Manager is an app that helps you manage and keep track of your daily appointments and schedules.
+    Part of it features is it ability to set reminder and being able to notify the user when the set date for
+    appointment reaches, via email and also on the dashboard.`,
+
+    image: '../../../assets/Images/portfolio-images/task-manager.png',
+
+    tech_used: ['Angular', 'Nestjs', 'HTML/CSS', 'Bootstrap', 'Angular Material'],
+    git_hub_link: '',
+    site_link: "https://ng-task-managers.netlify.app"
+  },
+  {
+    title: 'React Authentication ',
+    description: `
+            This is a Complete react App with routes guard that only authorized users can access certain components.
+            `,
+    image: '../../../assets/Images/portfolio-images/portfolio.png',
+    tech_used: ['React', 'Redux', 'Nest Js', 'HTML/CSS', 'Chakra UI'],
+    git_hub_link: "https://github.com/awasevalentine/react-full-authentication",
+    site_link:  "https://full-react-auth.netlify.app/dashboard"
+  },
+  {
+    title: 'Loan Request',
+    description: `
+              An App that is specifically design for those with <span>House Rent</span>
+              issues. We help you secure and renew your accomodation, and you payback by installment
+              `,
+    image: '../../../assets/Images/portfolio-images/loan-request.png',
+    tech_used: ['Angular', 'Nestjs', 'HTML/CSS', 'Bootstrap', 'Angular Material'],
+    git_hub_link: '',
+    site_link: "https://ng-loan-app.netlify.app"
+  },
+  {
+    title: 'Tweet',
+    description: `
+              Tweet is a replical of the twitter app that allows you to connect with friends and family,
+              send messages and as well receive updates.
+              `,
+    image: '../../../assets/Images/portfolio-images/twitter.png',
+    tech_used: ['Angular', 'Nestjs', 'HTML/CSS', 'Bootstrap', 'Angular Material'],
+    git_hub_link: '',
+    site_link: "https://ng-twitter.netlify.app"
+  }
+]
--- src/app/Pages/portfolio/portfolio.component.ts
+++ src/app/Pages/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PortfolioData} from './data';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+
+
+  public portfolioData = PortfolioData
+
+
+
   show = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.portfolioData.forEach((res)=>{
+      // JSON.stringify(res.tech_used) = JSON.stringify(res.tech_used).replace(',', ' ')
+    console.log("this is it ", JSON.stringify(res.tech_used).replace(',', ' '))
+
+    })
+
   }
 
 }
